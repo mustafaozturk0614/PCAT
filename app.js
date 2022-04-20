b55f2ab9db1a0c3b76ee88473da0d50a36f46317
musty1406 <musty1406@gmail.com>
2022-04-21 00:02:21 +0300

    static file uploded

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
-const fn = function () {
-    console.log('merhaba');
-};
+const express = require('express')
+const path = require('path')
+const app = express()
+
+
+app.use(express.static('public'))
+
+
+
+app.get('/', (req, res) => {
+
+    res.sendFile(path.resolve(__dirname, "temp/index.html"))
+})
+app.get('/about', (req, res) => {
+
+    res.status(200).send('About Sayfası')
+})
+app.get('/contact', (req, res) => {
+
+    res.status(200).send('contact Sayfası')
+})
+app.get('/*', (req, res) => {
+
+    res.status(404).send('Sayfa Bulunamadı')
+})
+const port = 3000
+
+app.listen(port, () => {
+
+    console.log(`Sunucu ${port} da çalışamaya başladı`)
+
+})
